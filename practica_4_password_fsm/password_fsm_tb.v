4dcce8f6911c2a094ee204bf26b7ddfe32ea9532
Santiago Benavent <A01646121@tec.mx>
2026-03-02 08:20:48 -0600

    uploud

--- practica_4_password_fsm/password_fsm_tb.v
+++ practica_4_password_fsm/password_fsm_tb.v
@@ -2,4 +2,15 @@ module password_fsm_tb(
     
 );
 
+ initial begin
+    $monitor("SW = %b",SW);
+end
+
+initial begin
+    $dumpfile("password_fsm_tb.vcd");
+    $dumpvars(0,password_fsm_tb);
+
+end
+      
+
 endmodule
