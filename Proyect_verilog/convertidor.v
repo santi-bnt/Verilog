fafc2a64f7fb38f6c6ebc1e81300e3198f57704e
Santiago Benavent <Santibenavale@gmail.com>
2026-03-06 08:27:39 -0600

    Refactor hvsync_generator and add convertidor module
    Refactored hvsync_generator module and added convertidor module for PWM control.

--- Proyect_verilog/convertidor.v
+++ Proyect_verilog/convertidor.v
@@ -1,58 +1,65 @@
-module hvsync_generator(
+module convertidor #(parameter pwm = 50,
+    parameter CLK_FREQ = 50_000_000)(
     input clk,
-    input pixel_tick,
-    output vga_h_sync,
-    output vga_v_sync,
-    output reg inDisplayArea,
-    output reg [9:0] CounterX=0,
-    output reg [9:0] CounterY=0
+    input rst,
+
+   input signed [15:0] data_x_reg,
+   input signed [15:0] data_y_reg,
+   input signed [15:0] data_z_reg,
+
+    output reg out_x,
+    output reg out_y,
+    output reg out_z
 );
 
-reg vga_HS=0;
-reg vga=VS=0;
+wire [15:0] count;
+reg  [15:0] comp_x;
+reg  [15:0] comp_y;
+reg  [15:0] comp_z;
 
-wire counterXmaxed= (CounterX==799);
-wire counterYmaxed= (CounterY==524);
+counter countersin(.rst(rst), .clk(clk), .counter(count));
 
-always @(posedge clk) begin
-    if (pixel_tick) begin
-        if (counterXmaxed)
-            CounterX <= 0;
+localparam integer min = ((CLK_FREQ/pwm)*3)/100;
+localparam integer max = ((CLK_FREQ/pwm)*12)/100;
+localparam integer m   = (max-min)/180;
+
+wire [7:0] angle_x;
+wire [7:0] angle_y;
+wire [7:0] angle_z;
+
+
+assign angle_x = 90 + (data_x_reg >>> 4); // desplaza el bit 4 a la derecha
+assign angle_y = 90 + (data_y_reg >>> 4);
+assign angle_z = 90 + (data_z_reg >>> 4);
+
+always @(posedge clk or posedge rst) begin 
+    if (rst) begin
+        out_x <= 0;
+        out_y <= 0;
+        out_z <= 0;
+    end 
+    else begin
+			
+        comp_x <= min + (angle_x * m);
+		  if (count < comp_x)
+            out_x <= 1;
         else
-            CounterX <= CounterX + 1;
-    end
-end
-
-always@(posedge clk) begin
-    if (pixel_tick && counterXmaxed) begin
-        if (counterYmaxed)
-            CounterY <= 0;
+            out_x <= 0;
+				
+        comp_y <= min + (angle_y * m);
+		   if (count < comp_y)
+            out_y <= 1;
         else
-            CounterY <= CounterY + 1;
+            out_y <= 0;
+				
+        comp_z <= min + (angle_z * m);
+		   if (count < comp_z)
+            out_z <= 1;
+        else
+            out_z <= 0;
+				
+
     end
 end
-
-always @(posedge clk) begin
-    if (pixel_tick) begin
-        vga_HS <= (counterX == 640 +16) && counterX < (640 + 16 + 96);
-    end
-end
-
-always @(posedge clk) begin
-    if (pixel_tick) begin
-        vga_VS <= (counterY == 480 + 10) && counterY < (480 + 10 + 2);
-    end
-end
-
-always @(posedge clk) begin
-    if (pixel_tick) begin
-        inDisplayArea <= (CounterX < 640) && (CounterY < 480);
-    end
-end
-
-assign vga_h_sync = vga_HS;
-assign vga_v_sync = vga_VS;
-
-
 
 endmodule
